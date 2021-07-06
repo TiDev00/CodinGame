faa483e074dee2831ef1b95cf1832999f116b109
TiDev00 <cisse.thierno@outlook.com>
2021-07-06 15:22:16 +0000

    firstCommit

--- EncodageSimple/EncodageSimple/Program.cs
+++ EncodageSimple/EncodageSimple/Program.cs
@@ -4,9 +4,36 @@ namespace EncodageSimple
 {
     class Program
     {
+        public static string Encode(string plainText)
+        {
+            if (plainText != null)
+            {
+                foreach (var letter in plainText)
+                {
+                    if (char.IsLetter(letter))
+                    {
+                        return "good";
+                    }
+                    else
+                    {
+                        return "String contains character which is not a letter";
+                    }
+                }
+            }
+            else
+            {
+                return "String is null";
+            }
+            return "";
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string plainText = Console.ReadLine();
+            var stdoutWriter = Console.Out;
+            Console.SetOut(Console.Error);
+            string encoded = Encode(plainText);
+            Console.SetOut(stdoutWriter);
+            Console.WriteLine(encoded);
         }
     }
 }
