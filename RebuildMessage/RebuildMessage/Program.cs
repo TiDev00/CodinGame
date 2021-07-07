5190836814b7cc473b6f7a3452cb578529776e4f
TiDev00 <cisse.thierno@outlook.com>
2021-07-07 16:19:05 +0000

    Rebuild initialization

---
+++ RebuildMessage/RebuildMessage/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace RebuildMessage
+{
+    class Program
+    {
+        public static string RebuildMessage(string[] parts)
+        {
+            return "";
+        }
+        
+        static void Main(string[] args)
+        {
+            string[] inputs;
+            int partsCount = int.Parse(Console.ReadLine());
+            string[] parts = new string[partsCount];
+            for (int i = 0; i < partsCount; i++)
+            {
+                parts[i] = Console.ReadLine();
+            }
+            var stdtoutWriter = Console.Out;
+            Console.SetOut(Console.Error);
+            string message = RebuildMessage(parts);
+            Console.SetOut(stdtoutWriter);
+            Console.WriteLine(message);
+        }
+    }
+}
