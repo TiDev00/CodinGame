4c3fec024dff5972ededbae03357f51acd97d53d
TiDev00 <cisse.thierno@outlook.com>
2021-07-07 15:51:13 +0000

    QuadraticF initialization

---
+++ QuadraticFunction/QuadraticFunction/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace QuadraticFunction
+{
+    class Program
+    {
+        public static int ApplyFunction(string quadraticFunction, int x)
+        {
+            return 0;
+        }
+        
+        static void Main(string[] args)
+        {
+            string quadraticFunction = Console.ReadLine();
+            int x = int.Parse(Console.ReadLine());
+            var stdtoutWriter = Console.Out;
+            Console.SetOut(Console.Error);
+            int result = ApplyFunction(quadraticFunction, x);
+            Console.SetOut(stdtoutWriter);
+            Console.WriteLine(result);
+        }
+    }
+}
