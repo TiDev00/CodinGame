71182645779503136fe6d2631419b53eba168004
TiDev00 <cisse.thierno@outlook.com>
2021-07-07 15:03:31 +0000

    Compute Initialization

---
+++ ComputeMultipleSum/ComputeMultipleSum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ComputeMultipleSum
+{
+    class Program
+    {
+        public static int ComputeMultipleSum(int n)
+        {
+            return 0;
+        }
+        
+        static void Main(string[] args)
+        { 
+            int n = int.Parse(Console.ReadLine());
+            var stdtoutWriter = Console.Out;
+            Console.SetOut(Console.Error);
+            int res = ComputeMultipleSum(n);
+            Console.SetOut(stdtoutWriter);
+            Console.WriteLine(res);
+        }
+    }
+}
