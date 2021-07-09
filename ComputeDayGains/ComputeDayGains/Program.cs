259c01d5d11eaf2ef97f4e62b528fe6c7e191727
TiDev00 <cisse.thierno@outlook.com>
2021-07-09 15:24:02 +0000

    Compute update

--- ComputeDayGains/ComputeDayGains/Program.cs
+++ ComputeDayGains/ComputeDayGains/Program.cs
@@ -4,9 +4,33 @@ namespace ComputeDayGains
 {
     class Program
     {
+        public static int ComputeDayGains(int nbSeats, int[] payingGuests, int[] guestMovements)
+        {
+            return 0;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] inputs;
+            int nbSeats = int.Parse(Console.ReadLine());
+            int nbGuests = int.Parse(Console.ReadLine());
+            int nbMovements = int.Parse(Console.ReadLine());
+            int[] payingGuests = new int[nbGuests];
+            inputs = Console.ReadLine().Split(' ');
+            for (int i = 0; i < nbGuests; i++)
+                {
+                payingGuests[i] = int.Parse(inputs[i]);
+                }
+            int[] guestMovements = new int[nbMovements];
+            inputs = Console.ReadLine().Split(' ');
+            for (int i = 0; i < nbMovements; i++)
+                {
+                guestMovements[i] = int.Parse(inputs[i]);
+                }
+            var stdtoutWriter = Console.Out;
+            Console.SetOut(Console.Error);
+            int res = ComputeDayGains(nbSeats, payingGuests, guestMovements);
+            Console.SetOut(stdtoutWriter);
+            Console.WriteLine(res);
         }
     }
 }
